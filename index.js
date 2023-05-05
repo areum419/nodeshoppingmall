e3ba449464dd5629014ed41eed5de1f53f298727
areumm <areumm914@gmail.com>
2023-05-05 10:13:16 +0900

    my first app server

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from "express";
+// 상수 const
+const app = express();
+
+// port
+const port = 8080;
+
+app.listen(port, console.log("server start"));
+
+// 코드 빌드순서.. js , java, phyton, rudy, ....
+// 1. 위에서 아래 방향으로 읽음
+// 2. "=" 기준으로 오른쪽에서 왼쪽으로 치환
+// 3. "." 하위 메소드 호출
+// 4. "," 는 그리고 라고 해석
+// 5. () 함수들의 모음으로 해석
